d09facde09ed69059ee432681fcc1a808f71c22c
bartoszboruta <bboruta.dev@gmail.com>
2020-02-24 01:27:59 +0100

    feat #1: add large screen styles

--- src/components/Main/Sections/Zabdos/Card.js
+++ src/components/Main/Sections/Zabdos/Card.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 import Image from '../../../shared/Image'
 
-export const Card = ({ description, fileName, title }) => {
+export const Card = ({ className, description, fileName, title }) => {
   return (
-    <div className="card">
+    <div className={className}>
       <Image maxWidth={600} alt="Gatsby in Space" filename={fileName} className="card__header-image" />
       <div className="card__body">
         <span className="card__description">{description}</span>
--- src/components/Main/Sections/Zabdos/index.js
+++ src/components/Main/Sections/Zabdos/index.js
@@ -11,12 +11,16 @@ export const Zabdos = () => {
         <div className="section__header-decoration" />
         <h3 className="section__header-title">The Zabdos Experience</h3>
         <p className="section__header-description">Whether it's your first flight or simply your latest, we work to anticipate your every need</p>
+        <VideoHref />
       </div>
       <div className="section__body">
-        <VideoHref />
-        <Card title="Buisness class" fileName="ex1.jpg" description="Arrive inspired" />
-        <Card title="Economy class" fileName="ex2.jpg" description="Expect the expectional" />
-        <Card title="Explore the Fleet in AR" fileName="ex3.jpg" description="Award winning technology" />
+        <div className="cards">
+          <div className="cards__group">
+            <Card className="card" title="Buisness class" fileName="ex1.jpg" description="Arrive inspired" />
+            <Card className="card -transformed" title="Economy class" fileName="ex2.jpg" description="Expect the expectional" />
+          </div>
+          <Card className="card -big" title="Explore the Fleet in AR" fileName="ex3.jpg" description="Award winning technology" />
+        </div>
       </div>
     </section>
   )
